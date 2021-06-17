2ed6e880d162ed328f5ac26744e680f6ba04109c
LAMHAMDI Aymane <Aymane.Lamhamdi@bordeaux-inp.fr>
2021-06-18 01:57:50 +0200

    Adding madlibs game

---
+++ madlibs.py
@@ -0,0 +1,14 @@
+"""
+Beginner Python Project by Aymdi
+Matlibs using string concatenation
+"""
+
+name=input("Name: ")
+verb=input("Verb: ")
+adj1=input("Adjective: ")
+adj2=input("Adjective: ")
+
+madlib=f"subsribe to {name}, he {verb} all the time. He's very {adj1} but not {adj2}"
+
+print(madlib)
+
